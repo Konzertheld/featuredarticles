39a4efe405ef67aef0be8f050c96c16a51274b90
Konzertheld <christian.gredig@googlemail.com>
2013-01-14 22:53:38 +0100

    Remove another unnecessary file

--- featuredarticles.php
+++
@@ -1,7 +0,0 @@
-<?php if(count($theme->featuredarticles)): ?>
-	<ul>
-		<?php foreach($theme->featuredarticles as $article): ?>
-		<li><?php echo $article->title; ?></li>
-		<?php endforeach; ?>
-	</ul>
-<?php endif; ?>
